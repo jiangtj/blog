8debccfcf379370d24599b657adeb2db37fa5ede
Mr.J <116749895@qq.com>
2022-05-06 11:18:12 +0800

    .

--- scripts/views.js
+++ scripts/views.js
@@ -7,14 +7,14 @@ const path = require('path');
 const injector = require('hexo-extend-injector2')(hexo);
 
 // gitter
-injector.register('body-end', `<script>
-((window.gitter = {}).chat = {}).options = {
-  room: 'jiangtj/Lobby',
-  activationElement: false
-};
-</script>`);
-injector.register('body-end', '<script src="https://sidecar.gitter.im/dist/sidecar.v1.js" async defer></script>');
-injector.register('js', path.resolve(hexo.base_dir, 'views/gitter.js'));
+// injector.register('body-end', `<script>
+// ((window.gitter = {}).chat = {}).options = {
+//   room: 'jiangtj/Lobby',
+//   activationElement: false
+// };
+// </script>`);
+// injector.register('body-end', '<script src="https://sidecar.gitter.im/dist/sidecar.v1.js" async defer></script>');
+// injector.register('js', path.resolve(hexo.base_dir, 'views/gitter.js'));
 
 injector.register('head_end', '<script data-ad-client="ca-pub-8243423215749776" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>');
 injector.register('head_end', fs.readFileSync(path.resolve(hexo.base_dir, 'views/head.html'), 'utf8'));
